23ed3bc345c592bbe2d08ccd0e945d9b00845f19
Iker Mariñelarena <iker@540deg.com>
2020-01-12 10:45:51 +0100

    [2] - Elasticsearch and Kibana added to Docker

--- src/Infrastructure/Controller/AddCommentController.php
+++ src/Infrastructure/Controller/AddCommentController.php
@@ -2,13 +2,20 @@
 
 namespace App\Infrastructure\Controller;
 
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AddCommentController
 {
-    public function __invoke()
+    public function __invoke(LoggerInterface $logger)
     {
         echo 'hello Iker';
+        $logger->info('I just got the logger !!!!!!!');
+        $logger->info('I just got the logger !!!!!!!');
+        $request = Request::createFromGlobals();
+        $logger->info($request->getContent());
+        $logger->info($request->getBaseUrl());
 
         return new Response('hiiiii');
     }
